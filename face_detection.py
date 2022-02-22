dba81724992242e5c3f09521444af7d8637cd6f4
emunac@gmail.com <emunac@gmail.com>
2022-02-22 10:46:00 +0200

    track faces & correct

--- face_detection.py
+++ face_detection.py
@@ -12,72 +12,107 @@ from tqdm import tqdm
 from tkinter import Tk, filedialog
 import subprocess
 import platform
+from datetime import datetime
+from pathlib import Path
 import pandas as pd
 from smile_net import Net
 
 
-class Face:
-    # center is numpy array, to present the center point [x,y]
-    def __init__(self, center, pred):
-        self.current_center = center
-        self.predictions = []
-        self.predictions.append(pred)
+class FaceObject:
+    # center is numpy array, to present the last center point [x,y]
+    def __init__(self, box: list, pred: int, idx: int, v_len: int):
+        self.current_center = np.array([(box[0] + box[2])/2, (box[1] + box[3])/2])
+        self.predictions_in_frames = [None] * v_len
+        self.predictions_in_frames[idx] = pred
+        self.coordinates_in_frames = [None] * v_len
+        self.coordinates_in_frames[idx] = box
 
 
-def assign(faces, boxes, predictions):
+def assign(face_objects_list: list, boxes, predictions, idx, v_len) -> list:
     max_assign_distance = 50
     if len(boxes) != len(predictions):
         print("error, faces and predictions should have same length")
-        return faces
+        return face_objects_list
 
     for box, prediction in zip(boxes, predictions):
         center = np.array([(box[0] + box[2])/2, (box[1] + box[3])/2])
 
         min_distance = max_assign_distance
         closest_face = None
-        for face in faces:
+        for face in face_objects_list:
             distance = np.linalg.norm(face.current_center - center)
             if distance < min_distance:
                 min_distance = distance
                 closest_face = face
+
         if closest_face:
             closest_face.current_center = center
-            closest_face.predictions.append(prediction)
+            closest_face.predictions_in_frames[idx] = prediction
+            closest_face.coordinates_in_frames[idx] = box
         else:
-            face = Face(center, prediction)
-            faces.append(face)
+            face = FaceObject(box, prediction, idx, v_len)
+            face_objects_list.append(face)
 
-    return faces
+    return face_objects_list
+
+
+def correct(face_objects_list: list):
+    min_frame_appearance = 10
+    window_size = 10
+    sensitivity = 0
+    for face in face_objects_list:
+        count_frame_appearance = sum(x is not None for x in face.predictions_in_frames)
+        if count_frame_appearance < min_frame_appearance:
+            face_objects_list.remove(face)
+            continue
+
+        for i, x in enumerate(face.predictions_in_frames):
+            stri = str(x.item()) if x is not None else ''
+            print('frame #', i, 'frame detect:', stri)
+            # print(x.item(), i)
+
+        corrected_predictions: list = face.predictions_in_frames
+        half_window_size = int(window_size/2)
+        for i in range(half_window_size, len(face.predictions_in_frames) - half_window_size):
+            if sum(x is None for x in
+                   face.predictions_in_frames[i - half_window_size: i + 1 + half_window_size]) > 0:
+                continue
+
+            majority = sum(face.predictions_in_frames[i - half_window_size: i + 1 + half_window_size])
+            if majority < half_window_size - sensitivity:
+                corrected_predictions[i] = 0
+            else:
+                corrected_predictions[i] = 1
+
+        face.predictions_in_frames = corrected_predictions
+
 
 image_size = 64
-
 mtcnn = MTCNN(image_size=image_size, margin=0, post_process=False)
 
 root = Tk()
 dir_path = '/home/emuna/PycharmProjects/SheCodes/examples/'
 filepath = filedialog.askopenfilename(initialdir=dir_path, title="Select file",
                                       filetypes=(("video files", "*.mp4"), ("all files", "*.*")))
-filename = os.path.basename(filepath)
-new_filepath = dir_path + filename + '_detacted.mp4'
+filename = Path(filepath).stem
+current_time = datetime.now().strftime("%d-%m-%Y %H:%M")
+new_filepath = dir_path + filename + '_' + current_time + '_detected.mp4'
 
 v_cap = cv2.VideoCapture(filepath)
 frame_width = int(v_cap.get(3))
 frame_height = int(v_cap.get(4))
 v_len = int(v_cap.get(cv2.CAP_PROP_FRAME_COUNT))
 
-out = cv2.VideoWriter(new_filepath, cv2.VideoWriter_fourcc(*'mp4v'),
-                      24, (frame_width, frame_height))
-
-
-SMILE_MODEL_PATH = "state_dict_model.pt"
+SMILE_MODEL_PATH = "state_dict_model_sensitivity.pt"
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 smile_reco = Net()
-smile_reco.load_state_dict(torch.load('state_dict_model.pt'))
+smile_reco.load_state_dict(torch.load(SMILE_MODEL_PATH, map_location=device))
 smile_reco.eval()
 
 transform = transforms.Compose([transforms.ToTensor(), transforms.Grayscale()])
-faces = []
+face_objects_list = []
 missed = []
-# Loop through video
+# Loop through video for prediction
 for i in tqdm(range(v_len)):
     # Load frame
     success, frame = v_cap.read()
@@ -87,28 +122,45 @@ for i in tqdm(range(v_len)):
 
     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     frame = Image.fromarray(frame)
-    # face = mtcnn(frame)
-    # face = transform(face)
-    #
-    # output = smile_reco(face[None, ...])
-    # _, predicted = torch.max(output.data, 1)
-    # predictions.append(predicted.item())
 
-    frame_predictions = []
-    boxes, probs = mtcnn.detect(frame, landmarks=False)
-    draw = ImageDraw.Draw(frame)
+    frame_faces = []
+
+    boxes, _ = mtcnn.detect(frame, landmarks=False)
+    if boxes is None:
+        continue
     for box in boxes:
         face = frame.crop(box).resize((image_size, image_size), Image.BILINEAR)
         face = transform(face)
-        output = smile_reco(face[None, ...])
-        _, predicted = torch.max(output.data, 1)
-        frame_predictions.append(predicted)
+        frame_faces.append(face)
 
-        color = 'green' if predicted else "red"
-        draw.rectangle(box.tolist(), outline=color, width=6)
+    output = smile_reco(torch.stack(frame_faces))
+    _, frame_predictions = torch.max(output.data, 1)
+    face_objects_list = assign(face_objects_list, boxes, frame_predictions, i, v_len)
 
-    faces = assign(faces, boxes, frame_predictions)
-    out.write(cv2.cvtColor(np.array(frame), cv2.COLOR_RGB2BGR))
+correct(face_objects_list)
+
+v_cap = cv2.VideoCapture(filepath)
+out = cv2.VideoWriter(new_filepath, cv2.VideoWriter_fourcc(*'mp4v'),
+                      24, (frame_width, frame_height))
+
+# Loop through video for drawing
+for i in tqdm(range(v_len)):
+    # Load frame
+    success, frame = v_cap.read()
+    if not success:
+        continue
+
+    for face in face_objects_list:
+        face: FaceObject
+        prediction = face.predictions_in_frames[i]
+        coordinates = face.coordinates_in_frames[i]
+        if prediction is not None:
+            pt_1 = int(coordinates[0]), int(coordinates[1])
+            pt_2 = int(coordinates[2]), int(coordinates[3])
+            color = (0, 255, 0) if prediction else (0, 0, 255)
+            cv2.rectangle(frame, pt1=pt_1, pt2=pt_2, color=color, thickness=4)
+
+    out.write(frame)
 
 # When everything done, release the video capture and video write objects
 v_cap.release()
@@ -116,13 +168,17 @@ out.release()
 
 
 # test smiling accuracy
-df = pd.read_csv('labels.csv')
-if filename in df.columns:
-    truth = df[[filename]].values.squeeze()
-    truth = np.delete(truth, missed)
-    accuracy = (truth == np.array(predictions)).sum()
-    accuracy = 100 * accuracy / v_len
-    print('accuracy = {}'.format(accuracy))
+# df = pd.read_csv('labels.csv')
+# if filename in df.file_name.values:
+#     labels_loc = df.loc[df.file_name == filename, 'face_1'].apply(eval).values[0]
+#     truth = np.zeros(labels_loc[-1])
+#     for i in range(0, len(labels_loc) - 1, 2):
+#         truth[labels_loc[i]+1 : labels_loc[i+1]] = [1] * (labels_loc[i+1] - labels_loc[i] - 1)
+#
+#     face_object: FaceObject = face_objects_list[0]
+#     accuracy = (truth == np.array(face_object.predictions_in_frames)).sum()
+#     accuracy = 100 * accuracy / v_len
+#     print('accuracy = {}'.format(accuracy))
 
 # open detected video
 if platform.system() == 'Darwin':       # macOS
--- smile_recognition.py
+++ smile_recognition.py
@@ -22,102 +22,103 @@ torch.manual_seed(2)
 
 
 class FaceDataset(Dataset):
-  def __init__(self, img_dir, transform=None):
-    self.img_dir = img_dir
-    self.img_paths = []
-    self.img_labels = []
-    self.transform = transform
+    def __init__(self, img_dir, transform=None):
+        self.img_dir = img_dir
+        self.img_paths = []
+        self.img_labels = []
+        self.transform = transform
 
-    positive_path = os.path.join(img_dir, "positives/positives7")
-    negative_path = os.path.join(img_dir, "negatives/negatives7")
-    for path in [positive_path, negative_path]:
-      for file in os.listdir(path):
-        if file.endswith(".jpg"):
-          image_path = os.path.join(path, file)
-          self.img_paths.append(image_path)
-          if path is positive_path:
-            self.img_labels.append(1)
-          else:
-            self.img_labels.append(0)
+        positive_path = os.path.join(img_dir, "positives/positives7")
+        negative_path = os.path.join(img_dir, "negatives/negatives7")
+        for path in [positive_path, negative_path]:
+            for file in os.listdir(path):
+                if file.endswith(".jpg"):
+                    image_path = os.path.join(path, file)
+                    self.img_paths.append(image_path)
+                    if path is positive_path:
+                        self.img_labels.append(1)
+                    else:
+                        self.img_labels.append(0)
 
-  def __len__(self):
-    return len(self.img_labels)
+    def __len__(self):
+        return len(self.img_labels)
 
-  def __getitem__(self, idx):
-    convert_tensor = transforms.ToTensor()
-    image = Image.open(self.img_paths[idx])
-    image = convert_tensor(image)
-    if self.transform:
-      image = self.transform(image)
+    def __getitem__(self, idx):
+        convert_tensor = transforms.ToTensor()
+        image = Image.open(self.img_paths[idx])
+        image = convert_tensor(image)
+        if self.transform:
+            image = self.transform(image)
 
-    return image, self.img_labels[idx]
+        return image, self.img_labels[idx]
 
 
 def generate_sampler(dataset, indices):
-  train_labels = []  # list of labels of any example in train set
-  class_counts = [0] * 2
-  num_samples = len(indices)
-  for idx in indices:
-    _, label = dataset[idx]
-    class_counts[label] += 1
-    train_labels.append(label)
+    train_labels = []  # list of labels of any example in train set
+    class_counts = [0] * 2
+    num_samples = len(indices)
+    for idx in indices:
+        _, label = dataset[idx]
+        class_counts[label] += 1
+        train_labels.append(label)
 
-  class_weights = [num_samples / class_counts[i] for i in range(len(class_counts))]
-  # give a weight for any example in the train set
-  weights = [class_weights[train_labels[i]] for i in range(num_samples)]
-  sampler = torch.utils.data.WeightedRandomSampler(torch.DoubleTensor(weights), num_samples=num_samples)
-  return sampler
+    class_weights = [num_samples / class_counts[i] for i in range(len(class_counts))]
+    # give a weight for any example in the train set
+    weights = [class_weights[train_labels[i]] for i in range(num_samples)]
+    sampler = torch.utils.data.WeightedRandomSampler(torch.DoubleTensor(weights), num_samples=num_samples)
+    return sampler
+
 
 def wrap_confusion_matrix(y_true, y_pred):
-  matrix = [] #tn, fp, fn, tp
-  matrix = confusion_matrix(y_true=y_true, y_pred=y_pred).ravel()
+    matrix = []     # tn, fp, fn, tp
+    matrix = confusion_matrix(y_true=y_true, y_pred=y_pred).ravel()
 
-  #expected
-  if len(matrix) == 4:
-    return matrix
+    # expected
+    if len(matrix) == 4:
+        return matrix
 
-  #else len = 1 which means that y_true=y_pred
-  elif y_true[0] == 0:
-    return [4, 0, 0, 0]
-  else:
-    return [0, 0, 0, 4]
+    # else len = 1 which means that y_true = y_pred
+    elif y_true[0] == 0:
+        return [4, 0, 0, 0]
+    else:
+        return [0, 0, 0, 4]
 
 
 def test_validation():
-  model.eval()
-  total = 0
-  accuracy = 0.0
-  true_positive = 0
-  true_negative = 0
-  false_positive = 0
-  false_negative = 0
+    model.eval()
+    total = 0
+    accuracy = 0.0
+    true_positive = 0
+    true_negative = 0
+    false_positive = 0
+    false_negative = 0
 
-  with torch.no_grad():
-    for images, labels in val_loader:
-      images = images.to(device)
-      labels = labels.to(device)
-      outputs = model(images)
-      _, predicted = torch.max(outputs.data, 1)
-      total += labels.size(0)
-      accuracy += (predicted == labels).sum().item()
+    with torch.no_grad():
+        for images, labels in val_loader:
+            images = images.to(device)
+            labels = labels.to(device)
+            outputs = model(images)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            accuracy += (predicted == labels).sum().item()
 
-      tn, fp, fn, tp = wrap_confusion_matrix(labels, predicted)
-      true_positive += tp
-      true_negative += tn
-      false_positive += fp
-      false_negative += fn
+            tn, fp, fn, tp = wrap_confusion_matrix(labels, predicted)
+            true_positive += tp
+            true_negative += tn
+            false_positive += fp
+            false_negative += fn
 
-  accuracy = (100 * accuracy / total)
-  sensitivity = true_positive/(true_positive + false_negative)
-  specificity = true_negative/(true_negative + false_positive)
-  return accuracy, sensitivity, specificity
+    accuracy = (100 * accuracy / total)
+    sensitivity = true_positive/(true_positive + false_negative)
+    specificity = true_negative/(true_negative + false_positive)
+    return accuracy, sensitivity, specificity
 
 
 img_dir = "../SMILEsmileD/SMILEs"
 
 transform = transforms.Compose([
-  transforms.RandomRotation(10),
-  transforms.ColorJitter(brightness=0.2)
+    transforms.RandomRotation(10),
+    transforms.ColorJitter(brightness=0.2)
 ])
 
 dataset = FaceDataset(img_dir, transform=transform)
@@ -131,6 +132,7 @@ train_batch_size = 128
 val_size = len(dataset) - train_size
 train_dataset, val_dataset = random_split(dataset, [train_size, val_size])
 
+
 sampler = generate_sampler(dataset, train_dataset.indices)
 
 train_loader = DataLoader(dataset=train_dataset, batch_size=train_batch_size, sampler=sampler)
@@ -142,29 +144,33 @@ model = Net()
 optimizer = optim.Adam(model.parameters(), lr=lr)
 
 max_accuracy = 0
-PATH = "state_dict_model.pt"
+max_sensitivity = 0
+PATH = "state_dict_model_sensitivity.pt"
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model.to(device)
 
+model.load_state_dict(torch.load(PATH, map_location=device))
+test_validation()
+
 for epoch in range(n_epochs):
-  model.train()
-  losses = []
-  for image, label in tqdm(train_loader):
-    image = image.to(device)
-    label = label.to(device)
-    output = model(image)
-    loss = F.nll_loss(output, label)
-    losses.append(loss.detach())
-    loss.backward()
-    optimizer.step()
-    optimizer.zero_grad()
+    model.train()
+    losses = []
+    for image, label in tqdm(train_loader):
+        image = image.to(device)
+        label = label.to(device)
+        output = model(image)
+        loss = F.nll_loss(output, label)
+        losses.append(loss.detach())
+        loss.backward()
+        optimizer.step()
+        optimizer.zero_grad()
 
-  accuracy, sensitivity, specificity = test_validation()
-  if accuracy > max_accuracy:
-    max_accuracy = accuracy
-    torch.save(model.state_dict(), PATH)
+    accuracy, sensitivity, specificity = test_validation()
+    if sensitivity > max_sensitivity:
+        max_sensitivity = sensitivity
+        torch.save(model.state_dict(), PATH)
 
-  print(epoch + 1, "lr =", lr, "loss_train:", torch.stack(losses).mean(),
-        "accuracy:", accuracy, "sensitivity:", sensitivity, "specificity:", specificity)
+    print(epoch + 1, "lr =", lr, "loss_train:", torch.stack(losses).mean(),
+          "accuracy:", accuracy, "sensitivity:", sensitivity, "specificity:", specificity)
 
