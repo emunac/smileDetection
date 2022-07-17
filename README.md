0514fbeae6545798dfe82f7fee3b55799c201b57
emunac@gmail.com <emunac@gmail.com>
2022-07-17 12:51:54 +0300

    run from terminal

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # smileDetection
+## This is a project that detects smiling faces in a video
+
+### Description
+Input: An .mp4 video file.
+
+Output: An .mp4 video file in the same resolution with
+a green bounding box around each smiling face,
+and a red bounding box around each not smiling face.
+
+To gain this I trained a CNN classifier that recognizes
+smiles and used a pretrained face detection model to
+detect faces in each frame in the video.
+
+The dataset I used for training is
+https://github.com/hromi/SMILEsmileD
+which contains 13,165 grayscale face-focused images of
+64*64 pixels each. 9475 of those examples are not smiling faces, while only 3690 are smiling.
+
+To extract faces in each frame I used the open source 
+https://github.com/timesler/facenet-pytorch that provides pretrained face detection model.
+
+TO DO: How to use this repo in your pc
--- smile_recognition.py
+++ smile_recognition.py
@@ -2,6 +2,7 @@ import torch
 from torch.utils.data import DataLoader
 from torch.utils.data.dataset import random_split
 import os
+import torchvision
 from torchvision import transforms
 from matplotlib import pyplot as plt
 import torch.nn.functional as F
@@ -9,6 +10,8 @@ import torch.optim as optim
 from tqdm import tqdm
 from smile_net import Net
 from smile_dataset import SmileDataset
+from torch.utils.tensorboard import SummaryWriter
+from itertools import product
 import model_utils
 
 torch.manual_seed(2)
@@ -28,6 +31,17 @@ print("One example from dataset:", dataset[0])
 plt.imshow(dataset[0][0].permute(1, 2, 0))
 plt.show()
 
+parameters = dict(
+    lr=[0.1, 0.01, 0.001, 0.0001],
+    train_batch_size=[32, 64, 128]
+)
+
+param_values = [v for v in parameters.values()]
+print(param_values)
+
+for lr, train_batch_size in product(*param_values):
+    print(lr, train_batch_size)
+
 train_size = int(0.8 * len(dataset))
 train_batch_size = 128
 val_size = len(dataset) - train_size
@@ -38,10 +52,19 @@ sampler = model_utils.generate_sampler(dataset, train_dataset.indices, 2)
 train_loader = DataLoader(dataset=train_dataset, batch_size=train_batch_size, sampler=sampler)
 val_loader = DataLoader(dataset=val_dataset, batch_size=20)
 
-n_epochs = 1000
+tb = SummaryWriter()
+
+
+n_epochs = 50
 lr = 0.001
 model = Net()
 optimizer = optim.Adam(model.parameters(), lr=lr)
+
+images, labels = next(iter(train_loader))
+grid = torchvision.utils.make_grid(images)
+tb.add_image("images", grid)
+tb.add_graph(model, images)
+tb.close()
 
 max_accuracy = 0
 max_sensitivity = 0
@@ -50,21 +73,24 @@ PATH = "state_dict_model_sensitivity.pt"
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model.to(device)
 
-# Check best practices
 for epoch in range(n_epochs):
     model.train()
     losses = []
-    for image, label in tqdm(train_loader):
-        image = image.to(device)
-        label = label.to(device)
-        output = model(image)
-        loss = F.nll_loss(output, label)
+    for images, labels in tqdm(train_loader):
+        images, labels = images.to(device), labels.to(device)
+        output = model(images)
+
+        loss = F.nll_loss(output, labels)
         losses.append(loss.detach())
         loss.backward()
         optimizer.step()
         optimizer.zero_grad()
 
     accuracy, sensitivity, specificity = model_utils.test_model(model, val_loader)
+    tb.add_scalar('Accuracy', accuracy, epoch)
+    tb.add_scalar('Sensitivity', sensitivity, epoch)
+    tb.add_scalar('Specificity', specificity, epoch)
+
     if sensitivity > max_sensitivity:
         max_sensitivity = sensitivity
         # torch.save(model.state_dict(), PATH)
