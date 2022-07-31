c6a68bf852bb637fd86291747a45f811cfba933d
emunac <emunac@gmail.com>
2022-07-31 13:57:55 +0300

    fix_tb

--- smile_recognition.py
+++ smile_recognition.py
@@ -1,6 +1,6 @@
 import torch
 from torch.utils.data import DataLoader
-from torch.utils.data.dataset import Subset
+from torch.utils.data.dataset import random_split
 import os
 from torchvision import transforms
 from torch import nn
@@ -9,12 +9,11 @@ from smile_net import Net
 from smile_dataset import SmileDataset
 from torch.utils.tensorboard import SummaryWriter
 from itertools import product
-from sklearn.model_selection import KFold
 import model_utils
 
 torch.manual_seed(2)
 
-
+PATH = '/models/'
 img_dir = '../SMILEsmileD/SMILEs'
 positive_path = os.path.join(img_dir, 'positives/positives7')
 negative_path = os.path.join(img_dir, 'negatives/negatives7')
@@ -27,7 +26,6 @@ transform = transforms.Compose([
 dataset = SmileDataset(img_dir, positive_path, negative_path, transform=transform)
 print(f'Numbers of images in dataset: {len(dataset)}')
 
-tb = SummaryWriter()
 
 parameters = dict(
     lr=[0.001, 0.003, 0.0001],
@@ -37,75 +35,58 @@ param_values = [v for v in parameters.values()]
 
 # Configuration options
 loss_function = nn.NLLLoss()
-k_folds = 5
 n_epochs = 50
 
-# For fold results
-results = {}
-id_run = 0
+train_size = int(0.8 * len(dataset))
+train_batch_size = 128
+val_size = len(dataset) - train_size
+train_dataset, val_dataset = random_split(dataset, [train_size, val_size])
+
+sampler = model_utils.generate_sampler(dataset, train_dataset.indices, 2)
+
+train_loader = DataLoader(dataset=train_dataset, batch_size=train_batch_size, sampler=sampler)
+val_loader = DataLoader(dataset=val_dataset, batch_size=20)
 
 for ixd, (lr, train_batch_size) in enumerate(product(*param_values)):
 
     print(f'start training for hyperparameters: lr = {lr}, batch size = {train_batch_size}')
-    kfold = KFold(n_splits=k_folds, shuffle=True)
+    print(f'run num: {ixd + 1}')
 
-    for fold, (train_ids, test_ids) in enumerate(kfold.split(dataset)):
+    model = Net()
+    optimizer = optim.Adam(model.parameters(), lr=lr)
 
-        print(f'run num: {id_run + 1} fold id: {fold + 1}')
+    comment = f' batch_size = {train_batch_size} lr = {lr}'
+    tb = SummaryWriter(comment=comment)
 
-        train_dataset = Subset(dataset=dataset, indices=train_ids)
-        test_dataset = Subset(dataset=dataset, indices=test_ids)
+    max_accuracy = 0
+    max_sensitivity = 0
 
-        train_sampler = model_utils.generate_sampler(dataset, train_dataset.indices, 2)
-        train_loader = DataLoader(dataset=train_dataset, batch_size=train_batch_size, sampler=train_sampler)
-        test_loader = DataLoader(dataset=test_dataset, batch_size=20)
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    model.to(device)
 
-        model = Net()
-        optimizer = optim.Adam(model.parameters(), lr=lr)
+    for epoch in range(n_epochs):
 
-        max_accuracy = 0
-        max_sensitivity = 0
-        PATH = 'state_dict_model_sensitivity.pt'
+        model_utils.train_epoch(model, device, train_loader, loss_function, optimizer)
+        accuracy, sensitivity, specificity = model_utils.test_model(model, device, val_loader)
+        tb.add_scalar('Accuracy', accuracy, epoch)
+        tb.add_scalar('Sensitivity', sensitivity, epoch)
+        tb.add_scalar('Specificity', specificity, epoch)
 
-        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-        model.to(device)
+        if sensitivity > max_sensitivity:
+            max_sensitivity = sensitivity
+            # torch.save(model.state_dict(), f'{PATH}_sensitivity_{epoch}')
 
-        for epoch in range(n_epochs):
+        if accuracy > max_accuracy:
+            max_accuracy = accuracy
 
-            model_utils.train_epoch(model, device, train_loader, loss_function, optimizer)
-            accuracy, sensitivity, specificity = model_utils.test_model(model, device, test_loader)
-            tb.add_scalar('accuracy', accuracy, epoch)
-            tb.add_scalar('sensitivity', sensitivity, epoch)
-            tb.add_scalar('specificity', specificity, epoch)
-
-            if sensitivity > max_sensitivity:
-                max_sensitivity = sensitivity
-                # torch.save(model.state_dict(), PATH)
-
-            if accuracy > max_accuracy:
-                max_accuracy = accuracy
-
-            print(
-                f' epoch {epoch + 1}, accuracy={accuracy:.2f}, sensitivity={sensitivity:.2}, specificity={specificity:.2f}')
-
-        results[fold] = max_accuracy
-        id_run += 1
-
-    # Print fold results
-    print(f'K-FOLD CROSS VALIDATION RESULTS FOR {k_folds} FOLDS')
-
-    sum = 0.0
-    for key, value in results.items():
-        print(f'Fold {key}: {value} %')
-        sum += value
-    print(f'Average: {sum/len(results.items())} %')
+        print(
+            f' epoch {epoch + 1}, accuracy={accuracy:.2f}, sensitivity={sensitivity:.2}, specificity={specificity:.2f}')
 
     tb.add_hparams(
         {'lr': lr, 'bsize': train_batch_size},
         {
-            'accuracy': accuracy,
-            'sensitivity': sensitivity,
-            'specificity': specificity,
+            'accuracy': max_accuracy,
+            'sensitivity': max_sensitivity,
         },
     )
 tb.close()
